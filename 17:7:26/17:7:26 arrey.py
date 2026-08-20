0efdad52d560a850da76cccd74e2ba4aaae172d5
Nihar Sheladiya <niharsheladiya1506@gmail.com>
2026-08-20 21:44:14 +0530

    Add files via upload

---
+++ 17:7:26/17:7:26 arrey.py
@@ -0,0 +1,37 @@
+# Arrey in Python
+
+# Create a 1D Arrey (list) with five integer element.
+
+# 2 Fector of defined in Arrey
+
+# Homogeneous Arrey
+
+num = [1 , 2 , 3 , 4 , 5]
+
+print("Arrey Element:")
+print(type(num))
+
+for i in num:
+    print(i)
+
+
+# Heterogeneous Arrey
+
+student = ["Nihar" , 18 , 100 , True]
+
+print(student)
+
+
+# Common Typecodes
+
+'''
+arrayIdentifierName = array(typecode , [initilizer])
+
+"i" : "integer"
+"f" : "float"
+"d" : "double"
+"u" :"Unicode Character"
+
+Note: Array cannot store heterogeneous data. if you need diffrent datatype together , use a list
+
+'''
